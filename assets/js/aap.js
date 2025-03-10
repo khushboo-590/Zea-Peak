79d63dcc0bf5754a133aedb4902de846f1fb34d9
khushboo-590 <khushbook4979@gmail.com>
2025-03-10 09:42:11 +0530

    section-2 responsive

--- assets/js/aap.js
+++ assets/js/aap.js
@@ -1,4 +1,37 @@
 $(document).ready(function () {
+    $('.silder').slick({
+        dots: true,
+        infinite: true,
+        speed: 250,
+        slidesToShow: 3,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        prevArrow: false,
+        nextArrow: false,
+        responsive: [
+
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },           
+
+        ]
+
+    });
+});
+
+
+$(document).ready(function () {
     $('.my-file').slick({
         dots: true,
         infinite: true,
